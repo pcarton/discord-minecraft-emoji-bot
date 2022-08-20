42ebef6142201953aad12974cb738741fe2712fb
Patrick Carton <patrick.d.carton@gmail.com>
2022-08-20 12:48:37 -0400

    Remove unneeded command

--- src/main.rs
+++ src/main.rs
@@ -86,29 +86,6 @@ impl EventHandler for Handler {
                             .required(true)
                     })
                 })
-                .create_application_command(|command| {
-                    command
-                        .name("numberinput")
-                        .description("Test command for number input")
-                        .create_option(|option| {
-                            option
-                                .name("int")
-                                .description("An integer from 5 to 10")
-                                .kind(CommandOptionType::Integer)
-                                .min_int_value(5)
-                                .max_int_value(10)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("number")
-                                .description("A float from -3.3 to 234.5")
-                                .kind(CommandOptionType::Number)
-                                .min_number_value(-3.3)
-                                .max_number_value(234.5)
-                                .required(true)
-                        })
-                })
         })
         .await;
 
