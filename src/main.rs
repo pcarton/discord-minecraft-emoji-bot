bd979cc3cfc8889e0f99a597fb6d1e923f05d88d
Patrick Carton <patrick.d.carton@gmail.com>
2022-08-20 12:26:31 -0400

    remove unneeded examples

--- src/main.rs
+++ src/main.rs
@@ -34,25 +34,6 @@ impl EventHandler for Handler {
                         "Please provide a valid user".to_string()
                     }
                 },
-                "attachmentinput" => {
-                    let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected attachment option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected attachment object");
-
-                    if let CommandDataOptionValue::Attachment(attachment) = options {
-                        format!(
-                            "Attachment name: {}, attachment size: {}",
-                            attachment.filename, attachment.size
-                        )
-                    } else {
-                        "Please provide a valid attachment".to_string()
-                    }
-                },
                 _ => "not implemented :(".to_string(),
             };
 
@@ -95,51 +76,6 @@ impl EventHandler for Handler {
                 })
                 .create_application_command(|command| {
                     command
-                        .name("welcome")
-                        .name_localized("de", "begrüßen")
-                        .description("Welcome a user")
-                        .description_localized("de", "Einen Nutzer begrüßen")
-                        .create_option(|option| {
-                            option
-                                .name("user")
-                                .name_localized("de", "nutzer")
-                                .description("The user to welcome")
-                                .description_localized("de", "Der zu begrüßende Nutzer")
-                                .kind(CommandOptionType::User)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("message")
-                                .name_localized("de", "nachricht")
-                                .description("The message to send")
-                                .description_localized("de", "Die versendete Nachricht")
-                                .kind(CommandOptionType::String)
-                                .required(true)
-                                .add_string_choice_localized(
-                                    "Welcome to our cool server! Ask me if you need help",
-                                    "pizza",
-                                    [("de", "Willkommen auf unserem coolen Server! Frag mich, falls du Hilfe brauchst")]
-                                )
-                                .add_string_choice_localized(
-                                    "Hey, do you want a coffee?",
-                                    "coffee",
-                                    [("de", "Hey, willst du einen Kaffee?")],
-                                )
-                                .add_string_choice_localized(
-                                    "Welcome to the club, you're now a good person. Well, I hope.",
-                                    "club",
-                                    [("de", "Willkommen im Club, du bist jetzt ein guter Mensch. Naja, hoffentlich.")],
-                                )
-                                .add_string_choice_localized(
-                                    "I hope that you brought a controller to play together!",
-                                    "game",
-                                    [("de", "Ich hoffe du hast einen Controller zum Spielen mitgebracht!")],
-                                )
-                        })
-                })
-                .create_application_command(|command| {
-                    command
                         .name("numberinput")
                         .description("Test command for number input")
                         .create_option(|option| {
@@ -158,18 +94,6 @@ impl EventHandler for Handler {
                                 .kind(CommandOptionType::Number)
                                 .min_number_value(-3.3)
                                 .max_number_value(234.5)
-                                .required(true)
-                        })
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("attachmentinput")
-                        .description("Test command for attachment input")
-                        .create_option(|option| {
-                            option
-                                .name("attachment")
-                                .description("A file")
-                                .kind(CommandOptionType::Attachment)
                                 .required(true)
                         })
                 })
