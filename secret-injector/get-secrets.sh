63e2288419d41ba2671909a48c79c9719727b134
Patrick Carton <patrick.d.carton@gmail.com>
2023-09-10 13:43:39 -0400

    update to secret injector

--- secret-injector/get-secrets.sh
+++ secret-injector/get-secrets.sh
@@ -1,7 +1,10 @@
-#!/bin/bash 
-mkdir /secrets
+#!/bin/bash
+
+DISCORD_API_TOKEN=`gcloud secrets versions access $DISCORD_API_TOKEN_VERSION --secret=cartonbot-discord-token`
+DISCORD_GUILD_ID=`gcloud secrets versions access $DISCORD_GUILD_ID_VERSION --secret=cartonbot-discord-guild-id`
+
 cat > /secrets/secrets.yaml <<EOF
 secrets:
-  discord_api_token: "`gcloud secrets versions access $DISCORD_API_TOKEN_VERSION --secret=cartonbot-discord-token`"
-  discord_guild_id: "`gcloud secrets versions access $DISCORD_GUILD_ID_VERSION --secret=cartonbot-discord-guild-id`"
+  discord_api_token: "$DISCORD_API_TOKEN"
+  discord_guild_id: "$DISCORD_GUILD_ID"
 EOF
